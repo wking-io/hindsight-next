3301f4334993d8c4fc3707e664a623841335483a
Will King <contact@wking.io>
2017-09-06 07:52:32 -0500

    Hide header if user is not logged in.

--- components/Header.js
+++ components/Header.js
@@ -2,49 +2,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import Logo from './Logo';
-import WelcomeLinks from './WelcomeLinks';
 
-const Header = ({ pathname, isUser }) => (
-  <header>
-    <Link prefetch href="/">
-      <a>
-        <Logo />
-      </a>
-    </Link>
-
-    {isUser ? (
-      <div>
-        <Link prefetch href="/">
-          <a className={pathname === '/' && 'is-active'}>Dashboard</a>
-        </Link>
-        <Link prefetch href="/retros">
-          <a className={pathname === '/retros' && 'is-active'}>Retros</a>
-        </Link>
-      </div>
-    ) : (
-      <div>
-        <Link prefetch href="/about">
-          <a className={pathname === '/about' && 'is-active'}>About</a>
-        </Link>
-        <WelcomeLinks />
-      </div>
-    )}
-
-    <style jsx>{`
-      header {
-        margin-bottom: 25px;
-      }
-      a {
-        font-size: 14px;
-        margin-right: 15px;
-        text-decoration: none;
-      }
-      .is-active {
-        text-decoration: underline;
-      }
-    `}</style>
-  </header>
-);
+const Header = ({ pathname, isUser }) =>
+  isUser && (
+    <header>
+      <Link prefetch href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <Link prefetch href="/">
+        <a className={pathname === '/' && 'is-active'}>Dashboard</a>
+      </Link>
+      <Link prefetch href="/retros">
+        <a className={pathname === '/retros' && 'is-active'}>Retros</a>
+      </Link>
+      <style jsx>{`
+        header {
+          margin-bottom: 25px;
+        }
+        a {
+          font-size: 14px;
+          margin-right: 15px;
+          text-decoration: none;
+        }
+        .is-active {
+          text-decoration: underline;
+        }
+      `}</style>
+    </header>
+  );
 
 Header.propTypes = {
   pathname: PropTypes.string.isRequired,
